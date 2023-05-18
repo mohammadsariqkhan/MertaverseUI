734c1ccfb13f1bd1aa4be779b725bf577bb8afe4
Mohammad Sariq Khan <mohsariqkhan2004@gmail.com>
2023-05-18 15:31:27 +0530

    inital commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,7 +17,7 @@ const Footer = () => (
       <div className="flex flex-col">
         <div className="mb-[50px] h-[2px] bg-white opacity-10"/> 
           <div className="flex items-center justify-between flex-wrap gap-4">
-            <h4 className="font-extrabold text-[24px] text-white">Metaversus</h4>
+            <h4 className="font-extrabold text-[24px] text-white">Metaversuse</h4>
             <p className="font-normal text-[14px] text-white opacity-50">copyright</p>
             <div className="flex gap-4">
               {socials.map((social) => (
